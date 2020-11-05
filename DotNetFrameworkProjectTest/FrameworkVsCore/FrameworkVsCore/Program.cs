51f361b55c9a2e0d3437fb6661d6c97eb67069ed
Michal Hejduk <michal3run@gmail.com>
2020-11-05 22:47:03 +0100

    FrameworkVsCore project

---
+++ DotNetFrameworkProjectTest/FrameworkVsCore/FrameworkVsCore/Program.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System;
+using System.Security.Cryptography;
+
+namespace FrameworkVsCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Md5VsSha256>();
+        }
+    }
+
+    [SimpleJob(RuntimeMoniker.Net472)]
+    [LegacyJitX64Job]
+    [MemoryDiagnoser]
+    public class Md5VsSha256
+    {
+        private SHA256 sha256 = SHA256.Create();
+        private MD5 md5 = MD5.Create();
+        private byte[] data;
+
+        [Params(1000, 10000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256() => sha256.ComputeHash(data);
+
+        [Benchmark]
+        public byte[] Md5() => md5.ComputeHash(data);
+    }
+}
